ffa75c0306586a10c34f2d2785595b6e53c83ff1
dvargas42 <dvargas.eng@gmail.com>
2022-03-13 23:13:32 -0300

    feat: implement ActiveLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,25 @@
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import Link from "next/link";
+import { ActiveLink } from "../ActiveLink";
+
+import { SignInButton } from "../SignInButton";
+
+import styles from "./styles.module.scss";
 
 export function Header() {
-  return(
+  return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
     </header>
-  )
+  );
 }
