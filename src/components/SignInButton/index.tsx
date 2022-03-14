6daab7b9734460b6efc10828f8cd74e1f12fb30b
dvargas42 <dvargas.eng@gmail.com>
2022-03-13 23:09:39 -0300

    feat: implement login session logical

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,30 @@
-import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi';
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.buttonContainer}>
+  const { data: session } = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.buttonContainer}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04D361" />
-      <span>
-        dvargas42
-      </span>
-      <FiX className={styles.closeIcon} color="#737380"/>
+      <span>{session.user.name}</span>
+      <FiX className={styles.closeIcon} color="#737380" />
     </button>
-  ):(
-    <button type="button" className={styles.buttonContainer}>
+  ) : (
+    <button
+      type="button"
+      className={styles.buttonContainer}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
-      <span>
-        Sign in with Github
-      </span>
+      <span>Sign in with Github</span>
     </button>
-  )
+  );
 }
