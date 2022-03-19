d56878c1ec41eb44a7fc91af333325292f279ba4
dvargas42 <dvargas.eng@gmail.com>
2022-03-19 00:12:06 -0300

    feat: implement nprogress loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,36 @@
+import { useEffect } from "react";
 import { AppProps } from "next/app";
-import { Header } from "../components/Header";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
 import { SessionProvider as NextAuthProvider } from "next-auth/react";
+
+import { Header } from "../components/Header";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`);
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <NextAuthProvider session={pageProps.session}>
       <Header />
