fc602b66d0a143fb9e536014701d2a92354e1a05
dvargas42 <dvargas.eng@gmail.com>
2022-03-20 16:27:35 -0300

    fix: remove useless priceId prop

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,12 @@
-import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { signIn, useSession } from "next-auth/react";
+
 import { api } from "../../services/api";
 import { getStripeJs } from "../../services/stripe-js";
+
 import styles from "./styles.module.scss";
 
-interface SubscribeButtonProps {
-  priceId: string;
-}
-
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+export function SubscribeButton() {
   const { data: session } = useSession();
   const router = useRouter();
 
