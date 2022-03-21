6e337fbf27a0d5e57db50913ebccc7571c7fa7f6
dvargas42 <dvargas.eng@gmail.com>
2022-03-20 23:05:34 -0300

    style: apply code style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
+import { GetStaticProps } from "next";
+import Head from "next/head";
 
-import { SubscribeButton } from '../components/SubscribeButton';
-import { stripe } from '../services/stripe';
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
 interface HomeProps {
   product: {
     priceId: string;
     amount: string;
-  }
+  };
 }
 
 export default function Home({ product }: HomeProps) {
@@ -21,42 +21,45 @@ export default function Home({ product }: HomeProps) {
       </Head>
       <main className={styles.container}>
         <section className={styles.hero}>
-          <span>👏  Hey, welcome</span>
+          <span>👏 Hey, welcome</span>
 
-          <h1>News about <br />the <span>React</span> world.</h1>
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
 
           <p>
-            Get acess to all the publications<br />
+            Get acess to all the publications
+            <br />
             <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
-  )
+  );
 }
-
 
 export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve(process.env.STRIPE_PRICE, {
-    expand: ['product']
-  })
+    expand: ["product"],
+  });
 
   const product = {
     priceId: price.id,
-    amount: new Intl.NumberFormat('en-US', {
+    amount: new Intl.NumberFormat("en-US", {
       style: "currency",
-      currency: "USD"
+      currency: "USD",
     }).format(price.unit_amount / 100),
-  }
+  };
 
   return {
     props: {
-      product
+      product,
     },
     revalidate: 60 * 60 * 24, // 24 hours
-  }
-}
+  };
+};
