c750f90106922bea15924a2e3cd9dc33e9f95071
dvargas42 <dvargas.eng@gmail.com>
2022-03-20 23:00:06 -0300

    :tada: initial commit

---
+++ src/components/Async/index.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export function Async() {
+  const [isButtonVisible, setIsButtonVisible] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsButtonVisible(false);
+    }, 1000);
+  });
+  return (
+    <div>
+      <div>Hello World</div>
+      {isButtonVisible && <button>Button</button>}
+    </div>
+  );
+}
